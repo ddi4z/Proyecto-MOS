3a5ffdf622a0fc805d4ceac2e8eae5be65c4733b
ddi4z <d.diazm@uniandes.edu.co>
2024-11-18 22:15:59 -0500

    avance carga de datos

--- optiimizacion/desarrollo.py
+++ optiimizacion/desarrollo.py
@@ -3,54 +3,92 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pyomo.environ import *
 from pyomo.opt import SolverFactory
+from haversine import haversine, Unit
+import os
+import requests
 
-def cargarCaso1():
-    clientes = pd.read_csv("Proyecto Seneca Libre/case_1_base/Clients.csv")
-    clientes = clientes.drop('DepotID', axis=1)
-    depositos = pd.read_csv("Proyecto Seneca Libre/case_1_base/Depots.csv")
-    vehiculos = pd.read_csv("Proyecto Seneca Libre/case_1_base/multi_vehicles.csv")
-    estaciones = pd.read_csv("Proyecto Seneca Libre/case_1_base/RechargeNodes.csv")
-    return clientes, depositos, vehiculos, estaciones
+
+def calcularDistanciaHarvesiana(matriz, conjuntoDatos1, conjuntoDatos2):
+    for i in range(len(conjuntoDatos1)):
+        for j in range(len(conjuntoDatos2)):
+            punto1 = (conjuntoDatos1['Latitude'][i], conjuntoDatos1['Longitude'][i])
+            punto2 = (conjuntoDatos2['Latitude'][j], conjuntoDatos2['Longitude'][j])
+            matriz[i][j] = haversine(punto1, punto2, unit=Unit.KILOMETERS)
+            
+
+def calcularDistanciaRuta(matriz, conjuntoDatos1, conjuntoDatos2):
+    for i in range(len(conjuntoDatos1)):
+        for j in range(len(conjuntoDatos2)):
+            punto1 = (conjuntoDatos1['Latitude'][i], conjuntoDatos1['Longitude'][i])
+            punto2 = (conjuntoDatos2['Latitude'][j], conjuntoDatos2['Longitude'][j])
+            endpoint = f'http://router.project-osrm.org/route/v1/driving/{punto1[1]},{punto1[0]};{punto2[1]},{punto2[0]}?overview=false'
+            respuesta = requests.get(endpoint)
+            distancia = respuesta.json()['routes'][0]['distance']
+            matriz[i][j] = distancia 
     
+    
+def calcularMatrizDistancia(matriz, conjuntoDatos1, conjuntoDatos2):
+    calcularDistanciaRuta(matriz[0], conjuntoDatos1, conjuntoDatos2)
+    calcularDistanciaHarvesiana(matriz[1], conjuntoDatos1, conjuntoDatos2)
 
-def cargarCaso2():
-    clientes = pd.read_csv("Proyecto Seneca Libre/case_2_5_clients_per_vehicle/Clients.csv")
-    depositos = pd.read_csv("Proyecto Seneca Libre/case_2_5_clients_per_vehicle/Depots.csv")
-    vehiculos = pd.read_csv("Proyecto Seneca Libre/case_2_5_clients_per_vehicle/Vehicles.csv")
-    estaciones = pd.read_csv("Proyecto Seneca Libre/case_2_5_clients_per_vehicle/RechargeNodes.csv")
-    return clientes, depositos, vehiculos, estaciones
-
-def cargarCaso3():
-    clientes = pd.read_csv("Proyecto Seneca Libre/case_3_big_distances_small_demands/Clients.csv")
-    depositos = pd.read_csv("Proyecto Seneca Libre/case_3_big_distances_small_demands/Depots.csv")
-    vehiculos = pd.read_csv("Proyecto Seneca Libre/case_3_big_distances_small_demands/Vehicles.csv")
-    estaciones = pd.read_csv("Proyecto Seneca Libre/case_3_big_distances_small_demands/RechargeNodes.csv")
-    return clientes, depositos, vehiculos, estaciones
-
-def cargarCaso4():
-    clientes = pd.read_csv("Proyecto Seneca Libre/case_4_capacitated_depots/Clients.csv")
-    depositos = pd.read_csv("Proyecto Seneca Libre/case_4_capacitated_depots/Depots.csv")
-    vehiculos = pd.read_csv("Proyecto Seneca Libre/case_4_capacitated_depots/Vehicles.csv")
-    estaciones = pd.read_csv("Proyecto Seneca Libre/case_4_capacitated_depots/RechargeNodes.csv")
-    return clientes, depositos, vehiculos, estaciones
-
+def obtenerMatricesDeDistancia(clientes, depositos, estaciones, ruta):
+    if os.path.exists(ruta + 'D_ai.npy'):
+        D_ai = np.load(ruta + 'D_ai.npy')
+        D_ij = np.load(ruta + 'D_ij.npy')
+        D_ei = np.load(ruta + 'D_ei.npy')
+        D_ae = np.load(ruta + 'D_ae.npy')
+        D_ef = np.load(ruta + 'D_ef.npy')
+        return D_ai, D_ij, D_ei, D_ae, D_ef
+                       
+    D_ai = np.zeros((2, len(depositos), len(clientes)))
+    D_ij = np.zeros((2, len(clientes), len(clientes)))
+    D_ei = np.zeros((2, len(estaciones), len(clientes)))
+    D_ae = np.zeros((2, len(depositos), len(estaciones)))
+    D_ef = np.zeros((2, len(estaciones), len(estaciones)))
+    
+    calcularMatrizDistancia(D_ai, depositos, clientes)
+    np.save(ruta + 'D_ai.npy', D_ai)
+    calcularMatrizDistancia(D_ij, clientes, clientes)
+    np.save(ruta + 'D_ij.npy', D_ij)
+    calcularMatrizDistancia(D_ei, estaciones, clientes)
+    np.save(ruta + 'D_ei.npy', D_ei)
+    calcularMatrizDistancia(D_ae, depositos, estaciones)
+    np.save(ruta + 'D_ae.npy', D_ae)
+    calcularMatrizDistancia(D_ef, estaciones, estaciones)
+    np.save(ruta + 'D_ef.npy', D_ef)
+    
+    return D_ai, D_ij, D_ei, D_ae, D_ef
+    
+def cargarCaso(rutaBase):
+    clientes = pd.read_csv(f"{rutaBase}Clients.csv")
+    depositos = pd.read_csv(f"{rutaBase}Depots.csv")
+    if rutaBase == "Proyecto Seneca Libre/case_1_base/":
+        vehiculos = pd.read_csv(f"{rutaBase}multi_vehicles.csv")
+    else:
+        vehiculos = pd.read_csv(f"{rutaBase}Vehicles.csv")
+    estaciones = pd.read_csv(f"{rutaBase}RechargeNodes.csv")
+    D_ai, D_ij, D_ei, D_ae, D_ef = obtenerMatricesDeDistancia(clientes, depositos, estaciones, rutaBase)
+    return clientes, depositos, vehiculos, estaciones, D_ai, D_ij, D_ei, D_ae, D_ef
+    
 def cargarCasoDePrueba():
     print("1. Caso base")
     print("2. Caso 5 clientes por vehiculo")
     print("3. Caso grandes distancias poca demanda")
     print("4. Depositos con capacidad")
-    caso = input("Digite el número del caso de prueba: ")
-    if caso == "1":
-        return cargarCaso1()
-    elif caso == "2":
-        return cargarCaso2()
-    elif caso == "3":
-        return cargarCaso3()
-    elif caso == "4":
-        return cargarCaso4()
+    caso = int(input("Digite el número del caso de prueba: "))
+    
+    rutas = [
+        "Proyecto Seneca Libre/case_1_base/",
+        "Proyecto Seneca Libre/case_2_5_clients_per_vehicle/",
+        "Proyecto Seneca Libre/case_3_big_distances_small_demands/",
+        "Proyecto Seneca Libre/case_4_capacitated_depots/"
+    ]
+
+    return cargarCaso(rutas[caso - 1])
 
 
-clientes, depositos, vehiculos, estaciones = cargarCasoDePrueba()
+clientes, depositos, vehiculos, estaciones, D_ai, D_ij, D_ei, D_ae, D_ef = cargarCasoDePrueba()
+
 
 # Cuadro 1: Parámetros de Costos y Eficiencia para Vehículo
 # Gas Car | Drone | Solar EV
@@ -87,10 +125,6 @@ T = RangeSet(1, 3)
 
 # Definición de parámetros
 
-C_cargaDiario = C_cargaMinuto* T_kgxvDiario
-C_distanciaDiario = C_km,t * D_viajeDiario
-C_tiempoDiario = C_viajeHora,t * T_viajeDiario
-
 
 # Variables de decisión
 X = Var(A, C, V, within=Binary)
@@ -105,3 +139,7 @@ H = Var(E, E, V, within=Binary)
 
 K = Var(V,T, within=Binary)
 S = Var(V, N, within=Binary)
+
+
+
+
