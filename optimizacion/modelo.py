c7f4266ea647de01373ff6b5a8f445e7b473c5cc
ddi4z <d.diazm@uniandes.edu.co>
2024-12-05 16:02:02 -0500

    nuevo ajuste distancias

--- optimizacion/modelo.py
+++ optimizacion/modelo.py
@@ -37,18 +37,42 @@ M.T = RangeSet(1, 3)
 
 
 # Variables de decisión
-M.X = Var(M.A, M.C, M.V, within=Binary)
-M.Y = Var(M.C, M.C, M.V, within=Binary)
-M.Z = Var(M.C, M.A, M.V, within=Binary)
 
+# Almacén - Cliente
+M.X = Var(M.A, M.C, M.V, within=Binary)
+M.Y = Var(M.C, M.A, M.V, within=Binary)
+
+# Cliente - Cliente
+M.Z = Var(M.C, M.C, M.V, within=Binary)
+
+# Cliente - Estación
 M.W = Var(M.C, M.E, M.V, within=Binary)
 M.U = Var(M.E, M.C, M.V, within=Binary)
-M.M = Var(M.A, M.E, M.V, within=Binary)
-M.L = Var(M.E, M.A, M.V, within=Binary)
-M.H = Var(M.E, M.E, M.V, within=Binary)
 
-M.K = Var(M.V,M.T, within=Binary)
+# Estación - Estación
+M.M = Var(M.E, M.E, M.V, within=Binary)
+
+# Estación - Almacén
+M.L = Var(M.E, M.A, M.V, within=Binary)
+M.H = Var(M.A, M.E, M.V, within=Binary)
+
+# Subtoures
 M.S = Var(M.V, M.N, within=Binary)
+
+
+def t_kg_v_diario():
+    x = sum(sum(sum(M.X[a,i,v] * DEMANDAS[i] for i in M.C) for a in M.A) for v in M.V)
+    z = sum(sum(sum(M.Z[i,j,v] * DEMANDAS[j] for i in M.C) for j in M.C) for v in M.V)
+    u = sum(sum(sum(M.U[e,i,v] * DEMANDAS[i] for i in M.C) for e in M.E) for v in M.V)
+    return x + z + u
+
+def d_viaje_diario_t():
+    xy = sum(sum(sum(sum())))
+    zz = sum(sum(sum(sum())))
+    wu = sum(sum(sum(sum())))
+    mm = sum(sum(sum(sum())))
+    lh = sum(sum(sum(sum())))
+    
 
 v = Visualizador(p.clientes, p.almacenes, p.estaciones)
 
