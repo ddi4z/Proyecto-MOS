0cc2ae76760a848d7353fec82ef03be378e6247a
Sara Cárdenas <ss.cardenas@uniandes.edu.co>
2024-12-06 12:15:23 -0500

    compresión ciclos modelo y estaciones de carga

--- optimizacion/modelo.py
+++ optimizacion/modelo.py
@@ -75,6 +75,10 @@ M.S = Var(M.V, M.N, within=Binary)
 def N():
     return sum(sum(sum(M.X[a,i,v] for i in M.C) for a in M.A) for v in M.V) + sum(sum(sum(M.H[a,e,v] for e in M.E) for a in M.A) for v in M.V) 
 
+# Cantidad de veces que se recarga un vehículo
+def C_veces_recarga(v):
+    return sum(sum(M.X[a,i,v] for i in M.C) for a in M.A) + sum(sum(M.U[e,i,v] for i in M.C) for e in M.E) + sum(sum(M.M[e,f,v] for f in M.E) for e in M.E) + sum(sum(M.L[e,a,v] + M.H[a,e,v] for e in M.E) for a in M.A) 
+
 # Variables dependientes
 
 def t_kg_v_diario():
@@ -99,10 +103,10 @@ def q_energia_diaria_t():
     return x + u + m + lh
 
 def t_recarga_diario_t():
-    x = sum(sum(sum(sum(M.X[a,i,v] * p.TIPOS_VEHICULO[v-1, t-1] * p.TIEMPOS_RECARGA_COMPLETA[t] for t in M.T) for i in M.C) for a in M.A) for v in M.V)
-    u = sum(sum(sum(sum(M.U[e,i,v] * p.TIPOS_VEHICULO[v-1, t-1] * p.TIEMPOS_RECARGA_COMPLETA[t] for t in M.T) for i in M.C) for e in M.E) for v in M.V)
-    m = sum(sum(sum(sum(M.M[e,f,v] * p.TIPOS_VEHICULO[v-1, t-1] * p.TIEMPOS_RECARGA_COMPLETA[t] for t in M.T) for f in M.E) for e in M.E) for v in M.V)
-    lh = sum(sum(sum(sum((M.L[e,a,v] + M.H[a,e,v]) * p.TIPOS_VEHICULO[v-1, t-1] * p.TIEMPOS_RECARGA_COMPLETA[t] for t in M.T) for a in M.A) for e in M.E) for v in M.V)
+    x = sum(sum(sum(sum(M.X[a,i,v] * p.TIPOS_VEHICULO[v-1, t-1] * p.TIEMPOS_RECARGA_COMPLETA[t - 1] for t in M.T) for i in M.C) for a in M.A) for v in M.V)
+    u = sum(sum(sum(sum(M.U[e,i,v] * p.TIPOS_VEHICULO[v-1, t-1] * p.TIEMPOS_RECARGA_COMPLETA[t - 1] for t in M.T) for i in M.C) for e in M.E) for v in M.V)
+    m = sum(sum(sum(sum(M.M[e,f,v] * p.TIPOS_VEHICULO[v-1, t-1] * p.TIEMPOS_RECARGA_COMPLETA[t - 1] for t in M.T) for f in M.E) for e in M.E) for v in M.V)
+    lh = sum(sum(sum(sum((M.L[e,a,v] + M.H[a,e,v]) * p.TIPOS_VEHICULO[v-1, t-1] * p.TIEMPOS_RECARGA_COMPLETA[t - 1] for t in M.T) for a in M.A) for e in M.E) for v in M.V)
     return x + u + m + lh
 
 # Función objetivo
@@ -133,7 +137,7 @@ def c_energia_diario():
     x = sum(sum(sum(sum(M.X[a,i,v] * p.TIPOS_VEHICULO[v -1,t - 1] * p.COSTOS_RECARGA_UNIDAD_ENERGIA[t - 1] * (p.RANGOS[v - 1] / p.EFICIENCIAS_ENERGETICAS[t - 1]) for t in M.T) for i in M.C) for a in M.A) for v in M.V)
     u = sum(sum(sum(sum(M.U[e,i,v] * p.TIPOS_VEHICULO[v - 1,t - 1] * p.COSTOS_RECARGA_UNIDAD_ENERGIA[t - 1] * (p.RANGOS[v - 1] / p.EFICIENCIAS_ENERGETICAS[t - 1]) for t in M.T) for e in M.E) for i in M.C) for v in M.V)
     m = sum(sum(sum(sum(M.M[e,f,v] * p.TIPOS_VEHICULO[v - 1,t - 1] * p.COSTOS_RECARGA_UNIDAD_ENERGIA[t - 1] * (p.RANGOS[v - 1] / p.EFICIENCIAS_ENERGETICAS[t - 1]) for t in M.T) for f in M.E) for e in M.E) for v in M.V)
-    lh = sum(sum(sum(sum((M.L[e,a,v] + M.H[a,e,v]) * p.TIPOS_VEHICULO[v - 1,t - 1] * p.COSTOS_RECARGA_UNIDAD_ENERGIA[t] * (p.RANGOS[v - 1] / p.EFICIENCIAS_ENERGETICAS[t - 1]) for t in M.T) for a in M.A) for e in M.E) for v in M.V)
+    lh = sum(sum(sum(sum((M.L[e,a,v] + M.H[a,e,v]) * p.TIPOS_VEHICULO[v - 1,t - 1] * p.COSTOS_RECARGA_UNIDAD_ENERGIA[t - 1] * (p.RANGOS[v - 1] / p.EFICIENCIAS_ENERGETICAS[t - 1]) for t in M.T) for a in M.A) for e in M.E) for v in M.V)
     return x + u + m + lh
 
 def c_tiempo_energia_diario():
@@ -158,103 +162,100 @@ M.FO = Objective(rule=costo_total, sense=minimize)
 
 # 2.7.1 Restricciones propias de clientes, almacenes y vehículos 
 
-M.abastecimiento = ConstraintList()
+M.abastecimientoEntrada = ConstraintList()
+M.abastecimientoSalida = ConstraintList()
 for i in M.C:
     # Abastecimiento único al cliente (entrada)
-    M.abastecimiento.add(sum(sum(M.X[a,i,v] for a in M.A) for v in M.V) + sum(sum(M.Z[j,i,v] for j in M.C) for v in M.V) + sum(sum(M.U[e,i,v] for e in M.E) for v in M.V) == 1)
-    # Abastecimiento único al cliente (salida): 
-    M.abastecimiento.add(sum(sum(M.Y[i,a,v] for a in M.A) for v in M.V) + sum(sum(M.Z[i,j,v] for j in M.C) for v in M.V) + sum(sum(M.W[i,e,v] for e in M.E) for v in M.V) == 1)
+    M.abastecimientoEntrada.add(sum(sum(M.X[a,i,v] for a in M.A) for v in M.V) + sum(sum(M.Z[j,i,v] for j in M.C) for v in M.V) + sum(sum(M.U[e,i,v] for e in M.E) for v in M.V) == 1)
+    # Abastecimiento único al cliente (salida)
+    M.abastecimientoSalida.add(sum(sum(M.Y[i,a,v] for a in M.A) for v in M.V) + sum(sum(M.Z[i,j,v] for j in M.C) for v in M.V) + sum(sum(M.W[i,e,v] for e in M.E) for v in M.V) == 1)
 
 # Capacidad de los almacenes
 M.capacidadAlmacen = ConstraintList()
 for a in M.A:
     M.capacidadAlmacen.add(sum(sum(p.DEMANDAS[i - 1] * M.X[a,i,v] for i in M.C) for v in M.V) <= p.CAPACIDADES_PRODUCTOS_ALMACENES[a - 1])
 
-# Capacidad de los vehículos
 M.capacidadVehiculo = ConstraintList()
+# Una restricción incluye a la otra
+M.rangoVehiculo = ConstraintList()
+M.rangoVehiculoRecargas = ConstraintList()
 for v in M.V:
+    # Capacidad de los vehículos
     M.capacidadVehiculo.add(sum(sum(p.DEMANDAS[i - 1] * M.X[a,i,v] for i in M.C) for a in M.A) + sum(sum(p.DEMANDAS[j - 1] * M.Z[i,j,v] for i in M.C) for j in M.C) + sum(sum(p.DEMANDAS[i - 1] * M.U[e,i,v] for i in M.C) for e in M.E) 
                             <= sum(p.TIPOS_VEHICULO[v - 1, t - 1] * p.CAPACIDADES_PRODUCTOS_VEHICULO[v - 1] for t in M.T))
-
-# Rango del vehículo sin tener en cuenta las recargas intermedias
-M.rangoVehiculo = ConstraintList()
-for v in M.V:
+    
+    # Una restricción incluye a la otra
+    # Rango del vehículo sin tener en cuenta las recargas intermedias
     M.rangoVehiculo.add(d_viaje_diario_t() <= p.RANGOS[v - 1])
 
-# Rango del vehículo con recargas intermedias
-
-
+    # Rango del vehículo con recargas intermedias
+    M.rangoVehiculoRecargas.add(d_viaje_diario_t() <= p.RANGOS[v - 1] * C_veces_recarga(v))
 
 # 2.7.2 Restricciones del grafo 
 
 # Prohibición de subtoures
-
 M.subtoures = ConstraintList()
 for v in M.V:
+    # Z_ijv
     for i in M.C:
         for j in M.C:
             if i != j:
                 M.subtoures.add(M.S[v,i] - M.S[v,j] + M.Z[i,j,v] * N() <= N() - 1)
                 
     for e in M.E:
+        # W_iev y U_eiv
         for i in M.C:
             M.subtoures.add(M.S[v,i] - M.S[v,indiceEstacion(e)] + M.W[i,e,v] * N() <= N() - 1)
             M.subtoures.add(M.S[v,indiceEstacion(e)] - M.S[v,i] + M.U[e,i,v] * N() <= N() - 1)
-    
+
+        # M_efv
         for f in M.E:
             if e != f:
                 M.subtoures.add(M.S[v,indiceEstacion(e)] - M.S[v,indiceEstacion(f)] + M.M[e,f,v] * N() <= N() - 1)
 
 # 2.7.3 Restricciones de los vehículos y los almacenes
 
-# Salida única del almacén (nodo de origen)
 M.salidaUnicaAlmacen = ConstraintList()
-for v in M.V:
-    M.salidaUnicaAlmacen.add(sum(sum(M.X[a,i,v] for i in M.C) for a in M.A) + sum(sum(M.H[a,e,v] for e in M.E) for a in M.A) <= 1)
-
-# Entrada única al almacén (nodo de destino)
 M.entradaUnicaAlmacen = ConstraintList()
-for v in M.V:
-    M.entradaUnicaAlmacen.add(sum(sum(M.Y[i,a,v] for i in M.C) for a in M.A) + sum(sum(M.L[e,a,v] for e in M.E) for a in M.A) <= 1)
-
-# Salida y vuelta a un almacén de un vehículo
 M.salidaYVuelta = ConstraintList()
 for v in M.V:
-    M.salidaYVuelta.add(sum(sum(M.X[a,i,v] for i in M.C) for a in M.A) + sum(sum(M.H[a,e,v] for e in M.E) for a in M.A) - sum(sum(M.Y[i,a,v] for i in M.C) for a in M.A) + sum(sum(M.L[e,a,v] for e in M.E) for a in M.A) == 0)
+    # Salida única del almacén (nodo de origen)
+    M.salidaUnicaAlmacen.add(sum(sum(M.X[a,i,v] for i in M.C) for a in M.A) + sum(sum(M.H[a,e,v] for e in M.E) for a in M.A) <= 1)
+    # Entrada única al almacén (nodo de destino)
+    M.entradaUnicaAlmacen.add(sum(sum(M.Y[i,a,v] for i in M.C) for a in M.A) + sum(sum(M.L[e,a,v] for e in M.E) for a in M.A) <= 1)
+    # Salida y vuelta a un almacén de un vehículo
+    M.salidaYVuelta.add(sum(sum(M.X[a,i,v] for i in M.C) for a in M.A) + sum(sum(M.H[a,e,v] for e in M.E) for a in M.A) - 
+                        sum(sum(M.Y[i,a,v] for i in M.C) for a in M.A) - sum(sum(M.L[e,a,v] for e in M.E) for a in M.A) == 0)
 
 # 2.7.4 Restricciones de los vehículos y los clientes
 
-# Entrada única al cliente
 M.entradaUnicaCliente = ConstraintList()
-for i in M.C:
-    for v in M.V:
-        M.entradaUnicaCliente.add(sum(M.X[a,i,v] for a in M.A) + sum(M.Z[j,i,v] for j in M.C) + sum(M.U[e,i,v] for e in M.E) <= 1)
-
-# Salida única del cliente
 M.salidaUnicaCliente = ConstraintList()
-for i in M.C:
-    for v in M.V:
-        M.salidaUnicaCliente.add(sum(M.Y[i,a,v] for a in M.A) + sum(M.Z[i,j,v] for j in M.C) + sum(M.W[i,e,v] for e in M.E) <= 1)
-
-# Entrada y salida del cliente
 M.entradaYSalidaCliente = ConstraintList()
 for i in M.C:
     for v in M.V:
-        M.entradaYSalidaCliente.add(sum(M.X[a,i,v] for a in M.A) + sum(M.Z[j,i,v] for j in M.C) + sum(M.U[e,i,v] for e in M.E) - sum(M.Y[i,a,v] for a in M.A) - sum(M.Z[i,j,v] for j in M.C) - sum(M.W[i,e,v] for e in M.E) == 0)
+        # Entrada única al cliente
+        M.entradaUnicaCliente.add(sum(M.X[a,i,v] for a in M.A) + sum(M.Z[j,i,v] for j in M.C) + sum(M.U[e,i,v] for e in M.E) <= 1)
+        # Salida única del cliente
+        M.salidaUnicaCliente.add(sum(M.Y[i,a,v] for a in M.A) + sum(M.Z[i,j,v] for j in M.C) + sum(M.W[i,e,v] for e in M.E) <= 1)
+        # Entrada y salida del cliente
+        M.entradaYSalidaCliente.add(sum(M.X[a,i,v] for a in M.A) + sum(M.Z[j,i,v] for j in M.C) + sum(M.U[e,i,v] for e in M.E) - 
+                                    sum(M.Y[i,a,v] for a in M.A) - sum(M.Z[i,j,v] for j in M.C) - sum(M.W[i,e,v] for e in M.E) == 0)
 
 # 2.7.5. Restricciones de los vehículos y las estaciones de carga
 
-# Entrada única a la estación
-
-
-
-# Salida única de la estación
-
-
-
-# Entrada y salida de la estación de recarga
-
-
+M.entradaUnicaEstacion = ConstraintList()
+M.saidaUnicaEstacion = ConstraintList()
+M.entradaYSalidaEstacion = ConstraintList()
+for e in M.E:
+    for v in M.V:
+        # Entrada única a la estación
+        M.entradaUnicaEstacion.add(sum(M.H[a,e,v] for a in M.A) + sum(M.W[i,e,v] for i in M.C) + sum(M.M[f,e,v] for f in M.E) <= 1)
+        # Salida única de la estación
+        M.saidaUnicaEstacion.add(sum(M.L[e,a,v] for a in M.A) + sum(M.U[e,i,v] for i in M.C) + sum(M.M[e,f,v] for f in M.E) <= 1)
+        # Entrada y salida de la estación de recarga
+        M.entradaYSalidaEstacion.add(sum(M.H[a,e,v] for a in M.A) + sum(M.W[i,e,v] for i in M.C) + sum(M.M[f,e,v] for f in M.E) -
+                                     sum(M.L[e,a,v] for a in M.A) - sum(M.U[e,i,v] for i in M.C) - sum(M.M[e,f,v] for f in M.E) == 0)
 
 # Solución del modelo con SCIP
 
@@ -264,7 +265,6 @@ solver.options['limits/time'] = 570
 solver.options['heuristics'] = 'aggressive'
 solver.options['limits/absgap'] = 10 
 result = solver.solve(M, tee=True)
-
 
 # Visualización de la solución
 
